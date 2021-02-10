71819ef1b0a74dd42ddf20bfb24caae99e71aebe
Jack DeVries <jdevries3133@gmail.com>
2021-02-09 21:38:02 -0500

    feat: remove backend scaffolding; not going there yet

--- backend/README.md
+++
@@ -1,3 +0,0 @@
-# Danart Backend
-
-REST API to support dynamic features of the mostly static site.
--- backend/lead_forms/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LeadFormsConfig(AppConfig):
-    name = 'lead_forms'
