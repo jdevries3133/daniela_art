6ca27c393b3e888d4a3d9339943263c5ccd5e87d
Jack DeVries <jdevries3133@gmail.com>
2022-03-20 14:01:51 -0400

    refactor IaC to use single container deployment module

--- infrastructure.tf
+++ infrastructure.tf
@@ -29,83 +29,12 @@ data "external" "git_describe" {
   program = ["sh", "scripts/git_describe.sh"]
 }
 
-resource "kubernetes_deployment" "danart" {
-  metadata {
-    name      = "danart-deployment"
-    namespace = kubernetes_namespace.danart.metadata[0].name
-  }
 
-  spec {
-    replicas = 1
+module "container-deployment" {
+  source  = "jdevries3133/container-deployment/kubernetes"
+  version = "0.2.0"
 
-    selector {
-      match_labels = {
-        app = "danart"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "danart"
-        }
-      }
-      spec {
-        container {
-          name  = "danart"
-          image = "jdevries3133/danart:${data.external.git_describe.result.output}"
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "danart" {
-  metadata {
-    name      = "danart-service"
-    namespace = kubernetes_namespace.danart.metadata[0].name
-  }
-
-  spec {
-    selector = {
-      app = "danart"
-    }
-    session_affinity = "ClientIP"
-    port {
-      port        = 8080
-      target_port = 80
-    }
-  }
-}
-
-resource "kubernetes_ingress_v1" "danart_ingress" {
-  metadata {
-    name      = "danart-ingress"
-    namespace = kubernetes_namespace.danart.metadata[0].name
-  }
-
-  spec {
-    ingress_class_name = "public"
-    tls {
-      hosts = ["danart.us"]
-    }
-    rule {
-      host = "danart.us"
-      http {
-        path {
-          path      = "/"
-          path_type = "Prefix"
-          backend {
-            service {
-              name = kubernetes_service.danart.metadata.0.name
-              port {
-                number = kubernetes_service.danart.spec.0.port.0.port
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
+  app_name = "danart"
+  container = "jdevries3133/danart:${data.external.git_describe.result.output}"
+  domain = "danart.us"
 }
